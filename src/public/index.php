b2700d4a4a6717b7d90eb098f5099674568c029f
priyabrata das <pdas544@gmail.com>
2025-07-14 19:51:49 +0530

    use exception handling and proper response code

--- src/public/index.php
+++ src/public/index.php
@@ -12,14 +12,23 @@ define('VIEW_PATH', __DIR__ . '/../Views');
 use App\Router;
 use App\Controllers\HomeController;
 use App\Controllers\InvoiceController;
+use App\View;
 
 $router = new Router();
 
-$router->get('/', [HomeController::class, 'index'])
-    ->get('/invoices', [InvoiceController::class, 'index'])
-    ->get('/invoices/create', [InvoiceController::class, 'create']);
+try {
 
-//Need to echo the return value of resolve to display the view
-// echo $router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']
+    $router->get('/', [HomeController::class, 'index'])
+        ->get('/invoices', [InvoiceController::class, 'index'])
+        ->get('/invoices/create', [InvoiceController::class, 'create']);
 
-echo $router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));;
+    //Need to echo the return value of resolve to display the view
+    // echo $router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']
+
+    echo $router->resolve($_SERVER['REQUEST_URI'], strtolower($_SERVER['REQUEST_METHOD']));;
+} catch (\Exception $e) {
+    http_response_code(404);
+    echo View::make('errors/404', [
+        'message' => $e->getMessage()
+    ]);
+}
