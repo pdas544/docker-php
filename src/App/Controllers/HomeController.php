72019ebaf527221b7df9016bcfe01759398e435e
priyabrata das <pdas544@gmail.com>
2025-07-30 23:11:53 +0530

    use transactions in PDO

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -15,6 +15,21 @@ class HomeController
         /***
          * host: pb-mysql : name of the mysql container
          * named parameters: not positional and order does not matter
+         *
+         * difference between bindValue() and bindParam()
+         * bindValue(placeholder, value): the value cannot be changed once supplied. value can be direct literal
+         * for eg:
+         * bindValue(':id',$id) OR bindValue(':id',1)
+         * statement 1;
+         * statement 2;
+         * $id = 2;      -- no effect in bindValue() method
+         *
+         * bindParam(placeholder, value): the value cannot be a literal. the value is passed as reference and can be changed at runtime.
+         * for eg:
+         * bindParam(':id', $id);
+         * statement 1;
+         * statement 2;
+         * $id = 2; -- value of $id is changed before executing the statement.
          */
         $options = [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
@@ -23,24 +38,46 @@ class HomeController
         ];
         try {
             $cn = new PDO('mysql:host=pb-mysql;dbname=app_db', 'root', 'root', $options);
-
-            $query = 'SELECT * FROM users WHERE id = :id';
-            $stmt = $cn->prepare($query);
-            $stmt->bindValue(':id', 1);
-            $stmt->execute();
-            $user = $stmt->fetch();
-            var_dump($user);
-
-//            foreach ($cn->query($query) as $row) {
-//                echo '<pre>';
-//                var_dump($row);
-//                echo '</pre>';
-//            }
         }
         catch (PDOException $e) {
             echo $e->getMessage();
         }
+        $name = 'John';
+        $email = 'jane@example.com';
+        $amount = 100;
 
+        try {
+            $cn->beginTransaction();
+
+            $newUserStmt = $cn->prepare('INSERT INTO users (name, email) 
+                                    VALUES (:name, :email)'
+            );
+
+            $newInvoiceStmt = $cn->prepare('INSERT INTO invoices (amount, user_id) 
+                            VALUES (:amount, :user_id)'
+            );;
+
+            $newUserStmt->execute(['name' => $name, 'email' => $email]);
+
+
+            $newInvoiceStmt->execute(['amount' => $amount, 'user_id' => $cn->lastInsertId()]);
+
+            $cn->commit();
+        }catch (PDOException $e) {
+            if( $cn->inTransaction()){$cn->rollBack();}
+        }
+
+        $fetchStmt = $cn->prepare('SELECT invoices.id AS invoice_id, amount, 
+                        name FROM invoices 
+                       INNER JOIN users 
+                       ON invoices.user_id = users.id
+                        WHERE email = :email');
+
+        $fetchStmt->execute(['email' => $email]);
+        $invoices = $fetchStmt->fetchAll();
+        echo '<pre>';
+        print_r($invoices);
+        echo '</pre>';
         return View::make('index');
         // echo 'test home';
     }
